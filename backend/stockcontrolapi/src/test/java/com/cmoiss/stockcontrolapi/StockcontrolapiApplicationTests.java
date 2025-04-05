f90f29ce7d74b7d5ba0b7bfda8b3def1a43ca0f3
Caio Souza <ocaiomoises@gmail.com>
2025-04-05 16:41:44 -0300

    Spring Boot init

---
+++ backend/stockcontrolapi/src/test/java/com/cmoiss/stockcontrolapi/StockcontrolapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmoiss.stockcontrolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockcontrolapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
