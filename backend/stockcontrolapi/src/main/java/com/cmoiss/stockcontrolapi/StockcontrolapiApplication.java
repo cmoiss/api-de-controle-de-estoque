5eda9a94ba4c50df039b7d2a1a68085fae8757df
Caio Souza <ocaiomoises@gmail.com>
2025-04-05 16:41:44 -0300

    Spring Boot init
    - Update application.yml

---
+++ backend/stockcontrolapi/src/main/java/com/cmoiss/stockcontrolapi/StockcontrolapiApplication.java
@@ -0,0 +1,13 @@
+package com.cmoiss.stockcontrolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockcontrolapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockcontrolapiApplication.class, args);
+	}
+
+}
