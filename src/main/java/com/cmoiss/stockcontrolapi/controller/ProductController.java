d7cb8249ecc227e38ad5bd2b30ef7eb7b60dd048
Caio Souza <ocaiomoises@gmail.com>
2025-04-21 16:51:50 -0300

    Update ProductController.java

--- src/main/java/com/cmoiss/stockcontrolapi/controller/ProductController.java
+++ src/main/java/com/cmoiss/stockcontrolapi/controller/ProductController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 import static org.springframework.http.HttpStatus.CREATED;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:5178")
 @RestController
 @RequestMapping("/products")
 public class ProductController {
